a1b29f015ae0aa45614c971f4e75e66ec9b5ee23
Divya-256 <76658875+Divya-256@users.noreply.github.com>
2024-09-09 00:09:09 +0530

    assignee dashboard updated

--- frontend_gm/src/App.js
+++ frontend_gm/src/App.js
@@ -4,6 +4,7 @@ import UserDashboard from './Components/UserDashboard/UserDashboard';
 import Signup from './Components/SignupPage/Signup';
 import LoginPage from './Components/LoginPage/LoginPage';
 import HomePage from './Components/HomePage/HomePage';
+import AssigneeDashboard from './Components/AssigneeDashboard/AssigneeDashboard'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
            <Route path='/login' element={<LoginPage/>}/>
            <Route path='/signup' element={<Signup/>}/>
            <Route path='/userDashboard' element={<UserDashboard/>}/>
+           <Route path='/assigneeDashboard' element={<AssigneeDashboard/>}/>
       </Routes>
      
     </div>
